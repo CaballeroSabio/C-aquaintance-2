71aedb192c07678be3cb18141c4ea2f1d83679c7
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 19:53:49 +0300

    add 008_IntroMethod

---
+++ 008_InrtoMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+int a1 = 12;
+int b1 = 12;
+int c1 = 12;
+int a2 = 12;
+int b2 = 12;
+int c2 = 12;
+int a3 = 12;
+int b3 = 12;
+int c3 = 12;
+
+int max = a1;
+if (c1>1)
