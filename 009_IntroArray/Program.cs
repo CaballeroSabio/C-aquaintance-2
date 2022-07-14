f6b74ea6c08e1a5d83f65eeda6038cb8d6bacbd9
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-14 16:24:28 +0300

    added 009_IntroArray and created the new code

---
+++ 009_IntroArray/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//               0   1    2    3   4   5   6    7     8
+int[] array = { 12, 13, 7001, 71, 19, 99, 36, 4713, 800 };
+array[0] = 12;
+// Console.WriteLine(array[4]);
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+// int a1 = 12;
+// int b1 = 13;
+// int c1 = 7001;
+// int a2 = 71;
+// int b2 = 19;
+// int c2 = 99;
+// int a3 = 36;
+// int b3 = 47123;
+// int c3 = 800;
+
+// int max = a1;
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+
+// if (a3 > max) max = a3;
+// if (b3 > max) max = b3;
+// if (c3 > max) max = c3;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+// int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
+Console.WriteLine();
