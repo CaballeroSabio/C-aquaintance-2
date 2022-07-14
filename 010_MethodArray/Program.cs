59d835c0eb893f89a3eff028ca462c9fc76ea60f
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-14 17:14:39 +0300

    added 010_MethodArray and created the new code

---
+++ 010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+int[] array = { 1, 12, 34, 57, 21, 11, 9, 42, 12 };
+
+int n = array.Length;
+int find = 12;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
